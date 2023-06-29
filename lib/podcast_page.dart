11df574cef78a4112cb7e64105b81c8129273803
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-06-29 21:03:47 +0300

    added rounded button

--- lib/podcast_page.dart
+++ lib/podcast_page.dart
@@ -16,7 +16,7 @@ class PodcastPageView extends StatelessWidget {
       appBar: AppBar(
         systemOverlayStyle: SystemUiOverlayStyle.dark,
       ),
-      backgroundColor: Colors.blueGrey[50],
+      backgroundColor: const Color.fromARGB(255, 221, 226, 229),
       body: Column(
         children: [
           Image.asset(imageLink),
@@ -24,13 +24,13 @@ class PodcastPageView extends StatelessWidget {
             padding: const EdgeInsets.all(20.0),
             child: _textStyle(titleMessage),
           ),
-          const SizedBox(
-            height: 20,
-          ),
+          const SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
             child: _paragraphStyle(paragraphMessage),
           ),
+          const SizedBox(height: 65),
+          _roundedButton(),
         ],
       ),
     );
@@ -48,17 +48,40 @@ class PodcastPageView extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _paragraphStyle(String message) {
-  return Text(
-    message,
-    textAlign: TextAlign.center,
-    style: const TextStyle(
-      color: Color.fromARGB(255, 48, 47, 47),
-      fontWeight: FontWeight.w500,
-      fontSize: 18,
-      height: 1.6,
-    ),
-  );
+  Widget _paragraphStyle(String message) {
+    return Text(
+      message,
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+        color: Color.fromARGB(255, 48, 47, 47),
+        fontWeight: FontWeight.w500,
+        fontSize: 18,
+        height: 1.6,
+      ),
+    );
+  }
+
+  Widget _roundedButton() {
+    return RawMaterialButton(
+      onPressed: () {
+        // Düğmeye basıldığında yapılacak işlemler
+      },
+      fillColor: Color.fromARGB(255, 105, 147, 81),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      constraints: const BoxConstraints(
+        minWidth: 0,
+        minHeight: 0,
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(14.0),
+        child: Icon(
+          Icons.arrow_forward,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
 }
